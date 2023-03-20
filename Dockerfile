d0f3bd7740e2f78ba948168a929b43d03b0baa66
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-03-21 05:38:00 +0630

    update

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.9-slim-buster
+
+# Install dependencies
+RUN pip install fastapi uvicorn opencv-python-headless numpy dlib face-recognition
+
+# Set working directory
+WORKDIR /app
+
+# Copy the code into the container
+COPY . .
+
+# Expose the required port
+EXPOSE 8000
+
+# Run the server with uvicorn
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ app.py
@@ -0,0 +1,88 @@
+# pip install fastapi uvicorn opencv-python-headless numpy dlib face-recognition
+
+
+from fastapi import FastAPI, File, UploadFile
+import face_recognition
+import os
+
+app = FastAPI()
+
+
+# curl -X POST -F "file=@/path/to/image.jpg" http://localhost:8000/learn-face
+# function learnFace(file) {
+#   const formData = new FormData();
+#   formData.append('file', file);
+#   return axios.post('http://localhost:8000/learn-face', formData, {
+#     headers: {
+#       'Content-Type': 'multipart/form-data'
+#     }
+#   });
+# }
+
+@app.post("/learn-face")
+async def learn_face(file: UploadFile = File(...)):
+    # Read the image file and convert it to a numpy array
+    contents = await file.read()
+    np_array = np.fromstring(contents, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    # Find the face in the image and learn it
+    face_locations = face_recognition.face_locations(image)
+    if len(face_locations) == 1:
+        face_encoding = face_recognition.face_encodings(
+            image, face_locations)[0]
+        # Save the face encoding to a file
+        file_path = os.path.join(os.getcwd(), "face_encodings.txt")
+        with open(file_path, "a") as f:
+            encoding_str = ",".join(str(val) for val in face_encoding)
+            f.write(encoding_str + "\n")
+        return {"message": "Face learned and saved to file successfully"}
+    else:
+        return {"error": "Could not find a single face in the image"}
+
+
+# curl -X POST -F "file=@/path/to/image.jpg" http://localhost:8000/match-face
+# function matchFace(file) {
+#   const formData = new FormData();
+#   formData.append('file', file);
+#   return axios.post('http://localhost:8000/match-face', formData, {
+#     headers: {
+#       'Content-Type': 'multipart/form-data'
+#     }
+#   });
+# }
+@app.post("/match-face")
+async def match_face(file: UploadFile = File(...)):
+    # Read the image file and convert it to a numpy array
+    contents = await file.read()
+    np_array = np.fromstring(contents, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    # Find the face in the image and encode it
+    face_locations = face_recognition.face_locations(image)
+    if len(face_locations) == 1:
+        face_encoding = face_recognition.face_encodings(
+            image, face_locations)[0]
+    else:
+        return {"error": "Could not find a single face in the image"}
+
+    # Load saved face encodings from file
+    file_path = os.path.join(os.getcwd(), "face_encodings.txt")
+    with open(file_path, "r") as f:
+        saved_encodings = f.read().splitlines()
+
+    # Convert saved encodings to numpy arrays
+    saved_encodings = [np.fromstring(encoding, sep=",")
+                       for encoding in saved_encodings]
+
+    # Compare face encoding to saved encodings
+    matches = face_recognition.compare_faces(saved_encodings, face_encoding)
+
+    if True in matches:
+        return {"message": "Face matched successfully"}
+    else:
+        return {"error": "Could not match the face to any saved encodings"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
