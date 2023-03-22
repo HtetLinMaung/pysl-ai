4150c0b0b38ba6e4953b9e7254322230c4d8c968
hlm <hlm@mit.com.mm>
2023-03-22 13:19:21 +0630

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from urllib.request import urlretrieve
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Form, UploadFile
 import face_recognition
 import os
 import numpy as np
@@ -16,19 +16,31 @@ from io import BytesIO
 app = FastAPI()
 
 
-# curl -X POST -F "file=@/path/to/image.jpg" http://localhost:8000/learn-face
-# function learnFace(file) {
-#   const formData = new FormData();
-#   formData.append('file', file);
-#   return axios.post('http://localhost:8000/learn-face', formData, {
-#     headers: {
-#       'Content-Type': 'multipart/form-data'
-#     }
-#   });
-# }
+# curl -X POST -H "Content-Type: multipart/form-data" \
+# -F "file=@/path/to/image.jpg" \
+# -F "label=John" \
+# "http://localhost:8000/learn-face"
+# const axios = require('axios');
+
+# const formData = new FormData();
+# formData.append('file', imageFile);
+# formData.append('label', 'John');
+
+# axios.post('http://localhost:8000/learn-face', formData, {
+#   headers: {
+#     'Content-Type': 'multipart/form-data'
+#   }
+# })
+# .then((response) => {
+#   console.log(response.data);
+# })
+# .catch((error) => {
+#   console.log(error);
+# });
+
 
 @app.post("/learn-face")
-async def learn_face(file: UploadFile = File(...)):
+async def learn_face(file: UploadFile = File(...), label: str = Form(...)):
     # Read the image file and convert it to a numpy array
     contents = await file.read()
     np_array = np.fromstring(contents, np.uint8)
@@ -39,14 +51,15 @@ async def learn_face(file: UploadFile = File(...)):
     if len(face_locations) == 1:
         face_encoding = face_recognition.face_encodings(
             image, face_locations)[0]
-        # Save the face encoding to a file
+        # Save the face encoding with label to a file
         file_path = os.path.join(os.getcwd(), "face_encodings.txt")
         with open(file_path, "a") as f:
             encoding_str = ",".join(str(val) for val in face_encoding)
-            f.write(encoding_str + "\n")
+            f.write(label + "," + encoding_str + "\n")
         return {"message": "Face learned and saved to file successfully"}
     else:
         return {"error": "Could not find a single face in the image"}
+
 
 # curl -X POST -H "Content-Type: application/json" -d '{"url": "https://example.com/image.jpg"}' http://localhost:8000/learn-face-v2
 # axios.post('http://localhost:8000/learn-face-v2', {
@@ -107,20 +120,26 @@ async def match_face(file: UploadFile = File(...)):
     else:
         return {"error": "Could not find a single face in the image"}
 
-    # Load saved face encodings from file
+    # Load saved face encodings and their labels from file
     file_path = os.path.join(os.getcwd(), "face_encodings.txt")
     with open(file_path, "r") as f:
         saved_encodings = f.read().splitlines()
 
-    # Convert saved encodings to numpy arrays
-    saved_encodings = [np.fromstring(encoding, sep=",")
-                       for encoding in saved_encodings]
+    # Extract labels and face encodings from saved encodings
+    labels, encodings = [], []
+    for encoding_str in saved_encodings:
+        label, encoding = encoding_str.split(":")
+        labels.append(label)
+        encoding_arr = np.fromstring(encoding, sep=",")
+        encodings.append(encoding_arr)
 
     # Compare face encoding to saved encodings
-    matches = face_recognition.compare_faces(saved_encodings, face_encoding)
+    matches = face_recognition.compare_faces(encodings, face_encoding)
 
     if True in matches:
-        return {"message": "Face matched successfully"}
+        # Get the label associated with the matched encoding
+        matched_label = labels[matches.index(True)]
+        return {"message": f"Face matched successfully with label {matched_label}"}
     else:
         return {"error": "Could not match the face to any saved encodings"}
 
